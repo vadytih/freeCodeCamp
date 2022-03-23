62e3e7a29d31854901ea66271a6f431a1b1bfc14
Vadim <vady.tih@gmail.com>
2022-03-23 17:23:01 +0300

    002-008

---
+++ JavaScript Algorithms and Data Structures Certification (300 hours)/Basic JavaScript/008 - Понимание чувствительности к регистру в переменных.js
@@ -0,0 +1,50 @@
+// Понимание чувствительности к регистру в переменных
+// В JavaScript все имена переменных и функций чувствительны к регистру. Это означает, что капитализация имеет значение.
+//
+//     MYVARне то же самое, что MyVarни myvar. Можно иметь несколько разных переменных с одинаковым именем, но с разным регистром. Настоятельно рекомендуется для ясности не использовать эту языковую функцию.
+//
+//     Лучшая практика
+//
+// Пишите имена переменных в JavaScript в camelCase . В camelCase имена переменных, состоящие из нескольких слов, имеют первое слово в нижнем регистре, а первая буква каждого последующего слова — заглавная.
+//
+//     Примеры:
+//
+// var someVariable;
+// var anotherVariableName;
+// var thisVariableNameIsSoLong;
+// Измените существующие объявления и назначения, чтобы в их именах использовался camelCase .
+//
+//     Не создавайте никаких новых переменных.
+//
+//     studlyCapVarдолжны быть определены и иметь значение 10.
+//
+// properCamelCaseдолжен быть определен и иметь значение строки A String.
+//
+//     titleCaseOverдолжны быть определены и иметь значение 9000.
+//
+// studlyCapVarследует использовать camelCase как в разделах объявлений, так и в разделах назначения.
+//
+//     properCamelCaseследует использовать camelCase как в разделах объявлений, так и в разделах назначения.
+//
+//     titleCaseOverследует использовать camelCase как в разделах объявлений, так и в разделах назначения.
+// ===
+
+// // Variable declarations
+// var StUdLyCapVaR;
+// var properCamelCase;
+// var TitleCaseOver;
+//
+// // Variable assignments
+// STUDLYCAPVAR = 10;
+// PRoperCAmelCAse = "A String";
+// tITLEcASEoVER = 9000;
+
+// Variable declarations
+var studlyCapVar;
+var properCamelCase;
+var titleCaseOver;
+
+// Variable assignments
+studlyCapVar = 10;
+properCamelCase = "A String";
+titleCaseOver = 9000;

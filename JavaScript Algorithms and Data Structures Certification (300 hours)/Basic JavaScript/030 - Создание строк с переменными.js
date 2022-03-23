707673942cca62e8626f34e14c1d0e231610ecfa
Vadim <vady.tih@gmail.com>
2022-03-23 18:16:09 +0300

    021 - 030

---
+++ JavaScript Algorithms and Data Structures Certification (300 hours)/Basic JavaScript/030 - Создание строк с переменными.js
@@ -0,0 +1,24 @@
+// Создание строк с переменными
+// Иногда вам нужно будет построить строку в стиле Mad Libs . Используя оператор конкатенации ( +), вы можете вставить одну или несколько переменных в строку, которую вы создаете.
+//
+//     Пример:
+//
+// const ourName = "freeCodeCamp";
+// const ourStr = "Hello, our name is " + ourName + ", how are you?";
+// ourStrбудет иметь значение строки Hello, our name is freeCodeCamp, how are you?.
+//
+//     Установите myNameстроку, равную вашему имени, и постройте между myStrстроками иmyNameMy name is and I am well!
+//
+//     myNameдолжен быть установлен в виде строки длиной не менее 3 символов.
+//
+//     Вы должны использовать два +оператора для сборки myStrвнутри myNameнего.
+
+// ===
+// // Only change code below this line
+// const myName = "";
+// const myStr = "";
+// ===
+
+// Only change code below this line
+const myName = "Vadim";
+const myStr = "My name is " + myName + " and I am well!";
